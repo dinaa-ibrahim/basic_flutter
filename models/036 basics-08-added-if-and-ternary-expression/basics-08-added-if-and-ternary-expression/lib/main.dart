bbb00989c02d9ec96a53e407b2a76d10b2da459e
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2023-03-02 03:39:07 +0200

    some basic models

--- models/036 basics-08-added-if-and-ternary-expression/basics-08-added-if-and-ternary-expression/lib/main.dart
+++ models/036 basics-08-added-if-and-ternary-expression/basics-08-added-if-and-ternary-expression/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './quiz.dart';
-import './result.dart';
+import './question.dart';
+import './answer.dart';
+
 // void main() {
 //   runApp(MyApp());
 // }
@@ -17,56 +18,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _questions = const [
+  final questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ],
+      'answers': ['Black', 'Red', 'Green', 'White'],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 3},
-        {'text': 'Snake', 'score': 11},
-        {'text': 'Elephant', 'score': 5},
-        {'text': 'Lion', 'score': 9},
-      ],
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
     },
     {
       'questionText': 'Who\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-        {'text': 'Max', 'score': 1},
-      ],
+      'answers': ['Max', 'Max', 'Max', 'Max'],
     },
   ];
   var _questionIndex = 0;
-  var _totalScore = 0;
 
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
+  void _answerQuestion() {
     // var aBool = true;
     // aBool = false;
-
-    _totalScore += score;
 
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
-    if (_questionIndex < _questions.length) {
+    if (_questionIndex < questions.length) {
       print('We have more questions!');
     } else {
       print('No more questions!');
@@ -86,13 +62,21 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-                questions: _questions,
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(
+                    questions[_questionIndex]['questionText'],
+                  ),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
               )
-            : Result(_totalScore, _resetQuiz),
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
